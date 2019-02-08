2653c8760f0d1a092fe2dac5842441caf6ae4870
Johan R-G <johan@usercise.com>
2019-02-08 15:34:24 +0000

    Update storybook to reflect current components

--- src/stories/index.js
+++ src/stories/index.js
@@ -2,22 +2,49 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
 
-import { Button, Welcome } from '@storybook/react/demo';
-
-storiesOf('Welcome', module).add('to Storybook', () => (
-  <Welcome showApp={linkTo('Button')} />
-));
+import Button from '../button/Button';
+import NumberButton from '../button/NumberButton';
+import FunctionButton from '../button/FunctionButton';
 
 storiesOf('Button', module)
-  .add('with text', () => (
-    <Button onClick={action('clicked')}>Hello Button</Button>
+  .add('with text', () => <Button callback={action('clicked')} title="Text" />)
+  .add('with text and value', () => (
+    <Button callback={action('clicked')} title="Text" buttonValue={2} />
+  ));
+
+storiesOf('Number Button', module)
+  .add('with number', () => (
+    <NumberButton
+      number={2}
+      updateInput={action('clicked')}
+      displayValue="12"
+      isEditing={false}
+    />
   ))
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
+  .add('with number and modifier class', () => (
+    <NumberButton
+      number={2}
+      updateInput={action('clicked')}
+      displayValue="12"
+      isEditing={false}
+      modifier="plus"
+    />
+  ));
+
+storiesOf('Function Button', module)
+  .add('with sign', () => (
+    <FunctionButton
+      call="+"
+      functionButtonUpdateValue={action('clicked')}
+      displayValue="+"
+    />
+  ))
+  .add('with sign and modifier class', () => (
+    <FunctionButton
+      call="+"
+      functionButtonUpdateValue={action('clicked')}
+      displayValue="+"
+      modifier="plus"
+    />
   ));
