24f9ec5c762f038e88926b689f3c2beea82e6de5
Johan R-G <johan@usercise.com>
2019-02-08 12:04:05 +0000

    Add a basic initial css grid based layout
    * Enable us to see what the app will look like once working

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import './App.css';
+import './Calculator.css';
 
 interface CalculatorProps {
   isLargeSize: boolean;
@@ -28,7 +28,26 @@ class Calculator extends Component<CalculatorProps, CalculatorState> {
           this.props.isLargeSize ? ' Calculator--large' : ''
         }`}
       >
-        {this.state.value}
+        <h2 className="Calculator-result">{this.state.value}</h2>
+        <div className="Calculator-inputs">
+          <button>C</button>
+          <button>/</button>
+          <button>*</button>
+          <button>-</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button className="Calculator-plus">+</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button className="Calculator-equal">=</button>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button className="Calculator-zero">0</button>
+          <button>.</button>
+        </div>
       </div>
     );
   }
