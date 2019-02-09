6508ce6cbc1267bb3321a45c6eb76c764ef1ede1
Johan R-G <johan@usercise.com>
2019-02-09 15:52:15 +0000

    Fix multiple equals showing wrong value, active state for buttons

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -25,7 +25,7 @@ class Calculator extends Component<CalculatorProps, CalculatorState> {
     this.state = {
       entryValue: '0',
       stateValue: 0,
-      lastFunctionCall: '+',
+      lastFunctionCall: '',
       isEditing: false,
       equalCall: null
     };
@@ -35,39 +35,52 @@ class Calculator extends Component<CalculatorProps, CalculatorState> {
     this.setState({ entryValue: newValue, isEditing: true });
   }
 
-  functionButtonUpdateValue(newFunction: string, isEditing: boolean) {
+  functionButtonUpdateValue(newFunction: string) {
+    const numberValue = parseFloat(this.state.entryValue);
     if ('C' === newFunction) {
       this.setState({
         entryValue: '0',
         stateValue: 0,
         isEditing: false,
-        lastFunctionCall: '+'
+        lastFunctionCall: '',
+        equalCall: null
       });
-    } else {
-      const numberValue = parseFloat(this.state.entryValue);
-      const newValue =
-        '=' === newFunction && this.state.equalCall
-          ? this.calculateFunction(
-              this.state.equalCall.func,
-              this.state.equalCall.value
-            )
-          : this.calculateFunction(this.state.lastFunctionCall, numberValue);
+    } else if ('=' === newFunction) {
+      const newValue = this.state.equalCall
+        ? this.calculateFunction(
+            this.state.equalCall.func,
+            this.state.equalCall.value
+          )
+        : this.calculateFunction(this.state.lastFunctionCall, numberValue);
       this.setState(oldState => ({
         stateValue: newValue,
         entryValue: newValue.toString(),
-        lastFunctionCall:
-          newFunction === '=' ? oldState.lastFunctionCall : newFunction,
-        equalCall:
-          newFunction === '='
-            ? { func: oldState.lastFunctionCall, value: numberValue }
-            : null,
-        isEditing
+        lastFunctionCall: newFunction,
+        equalCall: oldState.equalCall || {
+          func: oldState.lastFunctionCall,
+          value: numberValue
+        },
+        isEditing: false
+      }));
+    } else {
+      const newValue = this.calculateFunction(
+        this.state.lastFunctionCall,
+        numberValue
+      );
+      this.setState(oldState => ({
+        stateValue: newValue,
+        entryValue: newValue.toString(),
+        lastFunctionCall: newFunction,
+        equalCall: null,
+        isEditing: false
       }));
     }
   }
 
   calculateFunction(func: string, value: number) {
     switch (func) {
+      case '=':
+        return this.state.stateValue;
       case '-':
         return this.state.stateValue - value;
       case '*':
@@ -98,18 +111,21 @@ class Calculator extends Component<CalculatorProps, CalculatorState> {
             functionButtonUpdateValue={this.functionButtonUpdateValue.bind(
               this
             )}
+            isActive={this.state.lastFunctionCall === '/'}
           />
           <FunctionButton
             call="*"
             functionButtonUpdateValue={this.functionButtonUpdateValue.bind(
               this
             )}
+            isActive={this.state.lastFunctionCall === '*'}
           />
           <FunctionButton
             call="-"
             functionButtonUpdateValue={this.functionButtonUpdateValue.bind(
               this
             )}
+            isActive={this.state.lastFunctionCall === '-'}
           />
           <NumberButton
             number={7}
@@ -135,6 +151,7 @@ class Calculator extends Component<CalculatorProps, CalculatorState> {
               this
             )}
             modifier="plus"
+            isActive={this.state.lastFunctionCall === '+'}
           />
           <NumberButton
             number={4}
