0b4b0ef1b20ba1a33a557ae19ee971295fe12e71
Johan R-G <johan@usercise.com>
2019-02-09 13:05:05 +0000

    Tests for Button component

--- src/button/Button.test.tsx
+++ src/button/Button.test.tsx
@@ -1,39 +1,48 @@
 import React from 'react';
-import { shallow, render } from 'enzyme';
+import { mount, shallow, render } from 'enzyme';
 import Button from './Button';
 
-test('renders without crashing', () => {
-  expect(shallow(<Button title="test" callback={() => {}} />).text()).toEqual(
-    'test'
-  );
-});
+describe('<Button />', () => {
+  it('renders without crashing', () => {
+    expect(shallow(<Button title="test" callback={() => {}} />).text()).toEqual(
+      'test'
+    );
+  });
 
-test('title gets set', () => {
-  expect(shallow(<Button title="test" callback={() => {}} />).text()).toEqual(
-    'test'
-  );
+  it('title gets set', () => {
+    expect(shallow(<Button title="test" callback={() => {}} />).text()).toEqual(
+      'test'
+    );
 
-  expect(
-    shallow(<Button title="testy" callback={() => {}} />).text()
-  ).not.toEqual('test');
-});
+    expect(
+      shallow(<Button title="testy" callback={() => {}} />).text()
+    ).not.toEqual('test');
+  });
 
-test('Button to not have changed since test', () => {
-  const component = render(<Button title="test" callback={() => {}} />);
-  let tree = component.serialize();
-  expect(tree).toMatchSnapshot();
-});
+  it('Button DOM to not have changed', () => {
+    const component = render(<Button title="test" callback={() => {}} />);
+    let tree = component.serialize();
+    expect(tree).toMatchSnapshot();
+  });
 
-test('class name to match element', () => {
-  expect(
-    shallow(<Button title="test" callback={() => {}} />).hasClass('Button')
-  ).toBeTruthy();
-});
+  it('class name to match element', () => {
+    expect(
+      shallow(<Button title="test" callback={() => {}} />).hasClass('Button')
+    ).toBeTruthy();
+  });
 
-test('modifier gets set', () => {
-  expect(
-    shallow(
-      <Button title="test" callback={() => {}} modifier="test" />
-    ).hasClass('Button--test')
-  ).toBeTruthy();
+  it('modifier gets set', () => {
+    expect(
+      shallow(
+        <Button title="test" callback={() => {}} modifier="test" />
+      ).hasClass('Button--test')
+    ).toBeTruthy();
+  });
+
+  it('simulates click events', () => {
+    const onButtonClick = jest.fn();
+    const wrapper = mount(<Button title="test" callback={onButtonClick} />);
+    wrapper.find('button').simulate('click');
+    expect(onButtonClick).toHaveBeenCalled();
+  });
 });
